76f27e32287f29ff3e5ffd737fd46b7906550f14
Lokesh Gaddam <gaddamlokesh20@gmail.com>
2023-11-12 18:21:13 +0530

    scraping setup actions

--- components/Searchbar.tsx
+++ components/Searchbar.tsx
@@ -1,21 +1,64 @@
-"use client"
-import React from 'react'
+"use client";
+import React from "react";
+// import { scrapeAndStoreProduct } from '@/lib/actions';
+import { FormEvent, useState } from "react";
+
+const isValidAmazonProductURL = (url: string) => {
+  try {
+    const parsedURL = new URL(url);
+    const hostname = parsedURL.hostname;
+
+    if (
+      hostname.includes("amazon.com") ||
+      hostname.includes("amazon.") ||
+      hostname.endsWith("amazon")
+    ) {
+      return true;
+    }
+  } catch (error) {
+    return false;
+  }
+
+  return false;
+};
 
 function Searchbar() {
-    const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {}
-    return (
-    <form 
-    className="flex flex-wrap gap-4 mt-12"
-    onSubmit={handleSubmit}
-    >
-        <input type="text" placeholder='Enter product link'
-        className='searchbar-input' />
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const isValidLink = isValidAmazonProductURL(searchPrompt);
 
-        <button type="submit" className='searchbar-btn'>
-            Search
-        </button>
+    if (!isValidLink) return alert("Please provide a valid Amazon link");
+
+    try {
+      setIsLoading(true);
+
+      // Scrape the product page
+      const product = await scrapeAndStoreProduct(searchPrompt);
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+  return (
+    <form className="flex flex-wrap gap-4 mt-12" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={searchPrompt}
+        onChange={(e) => setSearchPrompt(e.target.value)}
+        placeholder="Enter product link"
+        className="searchbar-input"
+      />
+
+      <button
+        type="submit"
+        className="searchbar-btn"
+        disabled={searchPrompt === ""}
+      >
+        {isLoading ? "Searching..." : "Search"}
+      </button>
     </form>
-  )
+  );
 }
 
-export default Searchbar
+export default Searchbar;
